49b9a8152e0ecacd7953fc9e3d2e3d11c20ce5c1
kunwe <167782990+kunwe@users.noreply.github.com>
2026-09-22 22:23:43 +0200

    Add files via upload

---
+++ app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_educonnect_EduConnectApp.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.example.educonnect.EduConnectApp"
+)
+public final class _com_example_educonnect_EduConnectApp {
+}
